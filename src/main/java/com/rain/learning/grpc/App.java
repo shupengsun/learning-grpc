ec5c512da4b4d155f31c239a1435e89aae10de5d
shupengsun <sp_sun@163.com>
2018-05-29 18:00:55 +0800

    create

---
+++ src/main/java/com/rain/learning/grpc/App.java
@@ -0,0 +1,13 @@
+package com.rain.learning.grpc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
